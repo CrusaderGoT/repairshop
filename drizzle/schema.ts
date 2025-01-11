7ce65d491d62722f02665ead64fa6cf4411d9cc0
crusader <enememeka44@gmail.com>
2025-01-12 00:31:05 +0100

    added drizzle and neon

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -3,21 +3,52 @@ import {
   serial,
   text,
   timestamp,
-  uniqueIndex,
+  boolean,
+  varchar,
+  integer
 } from 'drizzle-orm/pg-core';
- 
-export const UsersTable = pgTable(
-  'users',
-  {
-    id: serial('id').primaryKey(),
-    name: text('name').notNull(),
-    email: text('email').notNull(),
-    image: text('image').notNull(),
-    createdAt: timestamp('createdAt').defaultNow().notNull(),
-  },
-  (users) => {
-    return [{
-      uniqueIdx: uniqueIndex('unique_idx').on(users.email),
-    }];
-  },
+
+import { relations } from 'drizzle-orm';
+
+export const customers = pgTable("customers", {
+  id: serial("id").primaryKey(),
+  firstName: varchar("first_name").notNull(),
+  email: varchar("email").notNull().unique(),
+  phone: varchar("phone").notNull().unique(),
+  address: varchar("address").notNull(),
+  city: varchar("city").notNull(),
+  state: varchar("state", { length: 2 }).notNull(),
+  zip: varchar("zip", { length: 10 }),
+  notes: text("notes"),
+  active: boolean("active").notNull().default(true),
+  created: timestamp("timestamp").notNull().defaultNow(),
+})
+
+export const tickets = pgTable("customers", {
+  id: serial("id").primaryKey(),
+  customerId: integer("customer_id").notNull().references(() => customers.id),
+  title: varchar("title").notNull(),
+  description: varchar("description"),
+  completed: boolean("completed").notNull().default(false),
+  tech: varchar("tech").notNull().default("unassigned"),
+  created: timestamp("timestamp").notNull().defaultNow(),
+  updated: timestamp("updated").notNull().defaultNow().$onUpdate(() => new Date),
+});
+
+
+// Relations
+
+export const customersRelations = relations(customers,
+  ({ many}) => ({
+    tickets: many(tickets),
+  })
+);
+
+export const ticketsRelations = relations(tickets,
+  ({ one }) => ({
+    customer: one(customers, {
+      fields: [tickets.customerId],
+      references: [customers.id],
+    }),
+  })
 );
